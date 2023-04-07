a0273bf73311ba2ee4b931d4aa658d756b66bb46
fitabigail <abidinu35@gmail.com>
2023-04-07 18:38:22 +0000

    Project setup and create a superuser

---
+++ madame_musette/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for madame_musette project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'madame_musette.settings')
+
+application = get_asgi_application()
