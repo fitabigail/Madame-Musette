486a638881809fe55cb436d841bb1317ecffa33f
fitabigail <abidinu35@gmail.com>
2023-05-14 12:46:20 +0000

    Added sizes to product model and size selector to product template

--- products/models.py
+++ products/models.py
@@ -30,6 +30,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
                                  blank=True) 
     image = models.ImageField(null=True, blank=True)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     in_stock = models.BooleanField(default=True)   
     is_customized = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
