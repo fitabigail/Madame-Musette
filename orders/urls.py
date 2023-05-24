b6ac061127f8eb82d8dee6f06d90f163fc4d30a3
fitabigail <abidinu35@gmail.com>
2023-05-24 16:59:35 +0000

    Added class methods to webhook handler and webhook view

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 
 urlpatterns = [
     path('', views.order, name='order'),
     path('order_complete/<order_number>', views.order_complete, name='order_complete'),
+    path('wh/', webhook, name='webhook'),
 ]
