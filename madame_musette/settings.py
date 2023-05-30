265e8a58804483a925508e10c49b4f74cba324bd
fitabigail <abidinu35@gmail.com>
2023-05-30 16:55:13 +0000

    Remove secret key and test the automatic deployment

--- madame_musette/settings.py
+++ madame_musette/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('SECRET_KEY', '')
-SECRET_KEY = '91&3u4si9k28=9q8o8)*%z2kd9&p^jd^f%3np-v5za)w4r94@='
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
