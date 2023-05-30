9059a46049111f75bc75a3e32efe1aa1a8ccbdea
fitabigail <abidinu35@gmail.com>
2023-05-30 17:51:40 +0000

    empty commit

--- madame_musette/settings.py
+++ madame_musette/settings.py
@@ -179,7 +179,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(os.path.normpath(BASE_DIR), "static")
+    os.path.join(os.path.normpath(BASE_DIR), "static",)
 )
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
