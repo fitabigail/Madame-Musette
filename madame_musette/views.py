1d54bc99179cf84b7af762b632781cc0cff34c96
fitabigail <abidinu35@gmail.com>
2023-06-13 16:38:02 +0000

    Fixed 404,500 path

--- madame_musette/views.py
+++ madame_musette/views.py
@@ -3,19 +3,19 @@ from django.shortcuts import render
 
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
+    return render(request, "404.html", status=404)
 
 
 def handler500(request):
     """ Custom 500 page """
-    return render(request, "errors/500.html", status=500)
+    return render(request, "500.html", status=500)
 
 
 def handler403(request, exception):
     """ Custom 403 page """
-    return render(request, "errors/403.html", status=403)
+    return render(request, "403.html", status=403)
 
 
 def handler405(request, exception):
     """ Custom 405 page """
-    return render(request, "errors/405.html", status=405)
+    return render(request, "405.html", status=405)
